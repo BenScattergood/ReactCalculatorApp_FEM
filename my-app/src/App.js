7013753992ef9bf866f21d0aa7a1af27f4cc3882
Ben Scattergood <ben.s1234@hotmail.co.uk>
2022-12-21 19:19:44 +0000

    js theme changer logic complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,56 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import React from 'react'
 
 export default function App() {
+  const [currentTheme, setCurrentTheme] = React.useState(1)
+
+  React.useEffect(() => {
+    const color = getComputedStyle(document.documentElement).getPropertyValue('--current-bg-100');
+    switch (currentTheme) {
+      case 1:
+        changePropertyColor("t1")
+        break;
+      case 2:
+        changePropertyColor("t2")
+        break;
+      case 3:
+        changePropertyColor("t3")
+        break;
+      default:
+        changePropertyColor("t1")
+    }
+
+    console.log(color)
+  }, [currentTheme])
+
+  function changePropertyColor(theme) {
+    document.documentElement.style.setProperty("--current-bg-100", `var(--${theme}-bg-100)`);
+    document.documentElement.style.setProperty("--current-bg-200", `var(--${theme}-bg-200)`);
+    document.documentElement.style.setProperty("--current-bg-300", `var(--${theme}-bg-300)`);
+    document.documentElement.style.setProperty("--current-keys-bg-reset", `var(--${theme}-keys-bg-reset)`);
+    document.documentElement.style.setProperty("--current-keys-shadow-reset", `var(--${theme}-keys-shadow-reset)`);
+    document.documentElement.style.setProperty("--current-keys-toggle-reset", `var(--${theme}-keys-toggle-reset)`);
+    document.documentElement.style.setProperty("--current-keys-bg-equals", `var(--${theme}-keys-bg-equals)`);
+    document.documentElement.style.setProperty("--current-keys-shadow-equals", `var(--${theme}-keys-shadow-equals)`);
+    document.documentElement.style.setProperty("--current-keys-toggle-equals", `var(--${theme}-keys-toggle-equals)`);
+    document.documentElement.style.setProperty("--current-keys-bg-regular", `var(--${theme}-keys-bg-regular)`);
+    document.documentElement.style.setProperty("--current-keys-shadow-regular", `var(--${theme}-keys-shadow-regular)`);
+    document.documentElement.style.setProperty("--current-keys-toggle-regular", `var(--${theme}-keys-toggle-regular)`);
+    document.documentElement.style.setProperty("--current-keys-text-regular", `var(--${theme}-keys-text-regular)`);
+    document.documentElement.style.setProperty("--current-keys-text-equals", `var(--${theme}-keys-text-equals)`);
+    document.documentElement.style.setProperty("--current-header", `var(--${theme}-header)`);
+  }
+
   return (
     <div className="container">
-      <Navbar />
+      <Navbar
+        setCurrentTheme={setCurrentTheme}
+        currentTheme={currentTheme}
+      />
+      <div className="wrapper screen">
+        <h3 className="screen__text">123,4</h3>
+      </div>
       <main className=""></main>
     </div>
 
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,4 +1,24 @@
-export default function Navbar() {
+export default function Navbar({ setCurrentTheme, currentTheme }) {
+
+    function toggleTheme() {
+        setCurrentTheme(previousTheme => {
+            return previousTheme === 3 ? 1 : ++previousTheme
+        })
+    }
+
+    function switchPosition() {
+        switch (currentTheme) {
+            case 1:
+                return "flex-start"
+            case 2:
+                return "center"
+            case 3:
+                return "flex-end"
+            default:
+                return "flex-start"
+        }
+    }
+
     return (
         <nav className="navbar wrapper">
             <h1 className='color navbar__heading'>calc</h1>
@@ -10,11 +30,12 @@ export default function Navbar() {
                         <p>2</p>
                         <p>3</p>
                     </div>
-                    <div className="theme__toggle__switch">
-                        <span></span>
+                    <div className={"theme__toggle__switch " + switchPosition()}
+                        onClick={toggleTheme}>
+                        <span id="toggle-ball"></span>
                     </div>
                 </div>
             </section>
-        </nav>
+        </nav >
     )
 }
